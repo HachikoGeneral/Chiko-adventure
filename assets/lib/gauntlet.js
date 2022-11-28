2b1ef28ae45965fe11bb701825efbd4213a69d6a
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-11-28 18:58:29 +0000

    Update gauntlet.js

--- assets/lib/gauntlet.js
+++ assets/lib/gauntlet.js
@@ -26,149 +26,11 @@ let el = function (id) {
 };
 
 var strongHandsManagerABI = ([{"constant":false,"inputs":[{"name":"_unlockAfterNDays","type":"uint256"}],"name":"create","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"strongHand","type":"address"}],"name":"CreatedGauntlet","type":"event"},{"constant":true,"inputs":[],"name":"isGodlyChad","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myGauntlet","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"strongHands","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]);
+
 var gauntletABI = ([{"constant":true,"inputs":[],"name":"creationDate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"withdrawDividends","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"buyWithBalance","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_howManyDays","type":"uint256"}],"name":"extendLock","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"unlockAfterNDays","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isLocked","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"buy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_toAddress","type":"address"},{"name":"_amountOfTokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"developer","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lockedUntil","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_amount","type":"uint256"}],"name":"sell","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"dividendsOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"reinvest","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_owner","type":"address"},{"name":"_unlockAfterNDays","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]);
 
-let strongHandsManagerAddress = '0xa728eee96415714f559d156a3ed402f9adf7884d'; // Mainnet
-// let strongHandsManagerAddress = '0xa728eee96415714f559d156a3ed402f9adf7884d'; // Testnet
-
-let strongHandsManagerContract = web3.eth.contract(strongHandsManagerABI);
-let strongHandsManagerInstance = strongHandsManagerContract.at(strongHandsManagerAddress);
-
-strongHandContract = web3.eth.contract(gauntletABI);
-let myStrongHandInstance;
-
-function startDapp() {
-    setInterval(myStrongHand, 3000);
-}
-
-function myStrongHand() {
-    strongHandsManagerInstance.isGodlyChad((error, isGodlyChad) => {
-        if (isGodlyChad) {
-            
-            $("#setupPanel").hide();
-            $("#myDepositPanel").show();
-            $('#myTimePanel').show();
-            $('#myControlsPanel').show();
-
-            strongHandsManagerInstance.myGauntlet((error, myStrongHandAddress) => {
-                el('#address').innerHTML = myStrongHandAddress;
-
-                myStrongHandInstance = strongHandContract.at(myStrongHandAddress);
-                
-                myStrongHandInstance.balanceOf((error, myB1VSBalance) => {
-                    el('#myb1vsbalance').innerHTML = web3.fromWei(myB1VSBalance, 'ether').toFixed(4);
-                });
-                
-                myStrongHandInstance.dividendsOf((error, myDividends) => {
-                    el('#mydividends').innerHTML = web3.fromWei(myDividends, 'ether').toFixed(4);
-                });
-                
-                myStrongHandInstance.isLocked((error, isLocked) => {
-                    if (isLocked) {
-                        myStrongHandInstance.lockedUntil((error, lockedUntil) => {
-                            var _until = new Date(lockedUntil * 1000);
-                            el('#mystatus').innerHTML = '<b class="text-danger">LOCKED</b>';
-                            el('#lockedUntil').innerHTML = (_until.getDate() + "/" + (_until.getMonth() + 1) + "/" + _until.getFullYear());
-                        });
-                    } else {
-                        $('#mySellPanel').show();
-                    }
-                });
-
-                myStrongHandInstance.unlockAfterNDays((error, unlockAfterNDays) => {
-                    el('#daysUntil').innerHTML = unlockAfterNDays;
-                });
-                myStrongHandInstance.creationDate((error, creationDate) => {
-                    var _date = new Date(creationDate * 1000);
-                    el('#created').innerHTML = (_date.getDate() + "/" + (_date.getMonth() + 1) + "/" + _date.getFullYear());
-                });
-            });
-
-        } else {
-            el('#address').innerHTML = ("Create a Gauntlet Today!");
-            
-            el('#myb1vsbalance').innerHTML = ("- B1VS");
-            el('#mydividends').innerHTML = ("- BNB");
-            
-            el('#mystatus').innerHTML = ('<strong class="text-warning">NO GAUNTLET</b>');
-            el('#lockedUntil').innerHTML = ('--/--/----');
-            el('#daysUntil').innerHTML = ('-');
-            el('#created').innerHTML = ('-');
-            
-            $("#myDepositPanel").hide();
-            $('#myTimePanel').hide();
-            $('#myControlsPanel').hide();
-            $('#mySellPanel').hide();
-            
-            $("#setupPanel").show();
-        }
-    });
-}
-
-function getStrong() {
-    strongHandsManagerInstance.create(el('#locktime').value, (error, result) => {
-        if (!error) {
-            alertify.success("Creating Gauntlet, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-function buyB1VS() {
-    myStrongHandInstance.buy({
-        value: web3.toWei(el('#buyamount').value, 'ether')
-    }, (error, result) => {
-        if (!error) {
-            alertify.success("Buying B1VS Shares, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-function withdrawDividends() {
-    myStrongHandInstance.withdrawDividends((error, result) => {
-        if (!error) {
-            alertify.success("Withdrawing Dividends, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-function reinvestDividends() {
-    myStrongHandInstance.reinvest((error, result) => {
-        if (!error) {
-            alertify.success("Reinvesting B1VS Shares, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-function extendLock() {
-    myStrongHandInstance.extendLock(el('#extendlocktime').value, (error, result) => {
-        if (!error) {
-            alertify.success("Extending Lock, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-function sell() {
-    myStrongHandInstance.sell(web3.toWei(el('#sellamount').value, 'ether'), (error, result) => {
-        if (!error) {
-            alertify.success("Selling B1VS Shares, please wait...");
-        } else {
-            alertify.error("Failed - Try again or check Tx...")
-        }
-    });
-}
-
-let strongHandsManagerAddress = '0xa728eee96415714f559d156a3ed402f9adf7884d'; // Mainnet
-// let strongHandsManagerAddress = '0x1AB23a3C2d362Bd8BF14770Ed4514aDa1f076cE2'; // Testnet
+let strongHandsManagerAddress = '0x35e3ff1ca0360ccf8f6291e8a26a1282a9a4a87f'; // Mainnet
+// let strongHandsManagerAddress = '0x35e3ff1ca0360ccf8f6291e8a26a1282a9a4a87f'; // Testnet
 
 let strongHandsManagerContract = web3.eth.contract(strongHandsManagerABI);
 let strongHandsManagerInstance = strongHandsManagerContract.at(strongHandsManagerAddress);
@@ -226,7 +88,7 @@ function myStrongHand() {
         } else {
             el('#address').innerHTML = ("Create a Gauntlet Today!");
             
-            el('#mygttbalance').innerHTML = ("- GTT");
+            el('#myb1vsbalance').innerHTML = ("- GTT");
             el('#mydividends').innerHTML = ("- CHK");
             
             el('#mystatus').innerHTML = ('<strong class="text-warning">NO GAUNTLET</b>');
@@ -254,7 +116,7 @@ function getStrong() {
     });
 }
 
-function buyGTT() {
+function buyB1VS() {
     myStrongHandInstance.buy({
         value: web3.toWei(el('#buyamount').value, 'ether')
     }, (error, result) => {
